fe8a595c1241c3bd0497dc1dc7aa865bdde43d66
JackOefnLad <qyrtchy@gmail.com>
2024-05-04 12:47:09 +0300

    random values fo email&pass with UUID

--- Autom_JS/First Practice/page-objects/SignUp.js
+++ Autom_JS/First Practice/page-objects/SignUp.js
@@ -1,6 +1,7 @@
 import { expect } from "@playwright/test";
+import { v4 as uuidv4 } from "uuid"; //v4 as uuidv4 - переименование v4 в uuidv4
 
-const user = require("../data/user.json");
+//const user = require("../data/user.json");/
 
 export class SignUp {
   constructor(page) {
@@ -11,11 +12,20 @@ export class SignUp {
   }
 
   signUpNewUser = async () => {
+    //let i = user[Math.floor(Math.random() * user.length)];
+    //With UUID
     await this.emailForm.waitFor();
-    await this.emailForm.fill(user.email);
+    const emailId = uuidv4();
+    const email = emailId + "@gmail.com";
+    await this.emailForm.fill(email);
 
     await this.passwordForm.waitFor();
-    await this.passwordForm.fill(user.password);
+    const pasId = uuidv4();
+    await this.passwordForm.fill(pasId);
+
+    //Form file
+    //await this.emailForm.fill(user[2].email);
+    //await this.passwordForm.fill(user[2].password);
 
     await this.regButton.waitFor();
     await this.regButton.click();
